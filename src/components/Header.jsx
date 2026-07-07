5a36ccb4199bc410d75a089c05eff5fc86b0e0fc
mahbuba-bithi <mahbubabithi2393@gmail.com>
2026-07-07 07:58:40 -0700

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import MenuItem from './MenuItem'; // path অনুযায়ী ঠিক করো
 import { AiFillHome } from 'react-icons/ai'; // উদাহরণস্বরূপ, হিরো আইকন ব্যবহার করা হয়েছে
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className=' flex justify-between items-center p-4 max-w-6xl '>
+      <div className='flex gap-4'>
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
-        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
 
-      <div>
-        {/* Other content */}
-      </div>
+      <Link href="/" className='flex gap-4'>
+       <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
+       <span className='text-xl hidden sm:inline '>clone</span>
+      </Link>
     </div>
   );
 }
